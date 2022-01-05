7d394fdd389ba5ef1bdd728d3de8d13703cfa7a8
unknown <66fatihavci@gmail.com>
2022-01-05 12:30:57 +0300

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,150 @@
+   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="ti-close" aria-hidden="true"></span></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row no-gutters">
+                            <div class="col-lg-6 offset-lg-3 col-12">
+                                <h4 style="margin-top:100px;font-size:14px; font-weight:500; color:#F7941D; display:block; margin-bottom:5px;">Eshop Free Lite</h4>
+                                <h3 style="font-size:30px;color:#333;">Currently You are using free lite Version of Eshop.<h3>
+                                <p style="display:block; margin-top:20px; color:#888; font-size:14px; font-weight:400;">Please, purchase full version of the template to get all pages, features and commercial license</p>
+                                <div class="button" style="margin-top:30px;">
+                                    <a href="https://wpthemesgrid.com/downloads/eshop-ecommerce-html5-template/" target="_blank" class="btn" style="color:#fff;">Buy Now!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <!-- Modal end -->
+    
+    <!-- Start Footer Area -->
+    <footer class="footer">
+        <!-- Footer Top -->
+        <div class="footer-top section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5 col-md-6 col-12">
+                        <!-- Single Widget -->
+                        <div class="single-footer about">
+                            <div class="logo">
+                                <a href="index.html"><img src="images/logo2.png" alt="#"></a>
+                            </div>
+                            <p class="text">Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue,  magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</p>
+                            <p class="call">Got Question? Call us 24/7<span><a href="tel:123456789">+0123 456 789</a></span></p>
+                        </div>
+                        <!-- End Single Widget -->
+                    </div>
+                    <div class="col-lg-2 col-md-6 col-12">
+                        <!-- Single Widget -->
+                        <div class="single-footer links">
+                            <h4>Information</h4>
+                            <ul>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Faq</a></li>
+                                <li><a href="#">Terms & Conditions</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">Help</a></li>
+                            </ul>
+                        </div>
+                        <!-- End Single Widget -->
+                    </div>
+                    <div class="col-lg-2 col-md-6 col-12">
+                        <!-- Single Widget -->
+                        <div class="single-footer links">
+                            <h4>Customer Service</h4>
+                            <ul>
+                                <li><a href="#">Payment Methods</a></li>
+                                <li><a href="#">Money-back</a></li>
+                                <li><a href="#">Returns</a></li>
+                                <li><a href="#">Shipping</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                        <!-- End Single Widget -->
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-12">
+                        <!-- Single Widget -->
+                        <div class="single-footer social">
+                            <h4>Get In Tuch</h4>
+                            <!-- Single Widget -->
+                            <div class="contact">
+                                <ul>
+                                    <li>NO. 342 - London Oxford Street.</li>
+                                    <li>012 United Kingdom.</li>
+                                    <li>info@eshop.com</li>
+                                    <li>+032 3456 7890</li>
+                                </ul>
+                            </div>
+                            <!-- End Single Widget -->
+                            <ul>
+                                <li><a href="#"><i class="ti-facebook"></i></a></li>
+                                <li><a href="#"><i class="ti-twitter"></i></a></li>
+                                <li><a href="#"><i class="ti-flickr"></i></a></li>
+                                <li><a href="#"><i class="ti-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- End Single Widget -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Footer Top -->
+        <div class="copyright">
+            <div class="container">
+                <div class="inner">
+                    <div class="row">
+                        <div class="col-lg-6 col-12">
+                            <div class="left">
+                                <p>Copyright © 2020 <a href="http://www.wpthemesgrid.com" target="_blank">Wpthemesgrid</a>  -  All Rights Reserved.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <div class="right">
+                                <img src="images/payments.png" alt="#">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- /End Footer Area -->
+ 
+    <!-- Jquery -->
+    <script src="{{asset('front/')}}/js/jquery.min.js"></script>
+    <script src="{{asset('front/')}}/js/jquery-migrate-3.0.0.js"></script>
+    <script src="{{asset('front/')}}/js/jquery-ui.min.js"></script>
+    <!-- Popper JS -->
+    <script src="{{asset('front/')}}/js/popper.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="{{asset('front/')}}/js/bootstrap.min.js"></script>
+    <!-- Color JS -->
+    <script src="{{asset('front/')}}/js/colors.js"></script>
+    <!-- Slicknav JS -->
+    <script src="{{asset('front/')}}/js/slicknav.min.js"></script>
+    <!-- Owl Carousel JS -->
+    <script src="{{asset('front/')}}/js/owl-carousel.js"></script>
+    <!-- Magnific Popup JS -->
+    <script src="{{asset('front/')}}/js/magnific-popup.js"></script>
+    <!-- Waypoints JS -->
+    <script src="{{asset('front/')}}/js/waypoints.min.js"></script>
+    <!-- Countdown JS -->
+    <script src="{{asset('front/')}}/js/finalcountdown.min.js"></script>
+    <!-- Nice Select JS -->
+    <script src="{{asset('front/')}}/js/nicesellect.js"></script>
+    <!-- Flex Slider JS -->
+    <script src="{{asset('front/')}}/js/flex-slider.js"></script>
+    <!-- ScrollUp{{asset('front/')}}/ JS -->
+    <script src="{{asset('front/')}}/js/scrollup.js"></script>
+    <!-- Onepage Nav JS -->
+    <script src="{{asset('front/')}}/js/onepage-nav.min.js"></script>
+    <!-- Easing JS -->
+    <script src="{{asset('front/')}}/js/easing.js"></script>
+    <!-- Active JS -->
+    <script src="{{asset('front/')}}/js/active.js"></script>
+</body>
+</html>
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>App Name - @yield('title')</title>
+    </head>
+    <body>
+        @yield('sidebar2')
+       
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
